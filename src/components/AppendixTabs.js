fa2bb398d30204e3c7d8548d1c04585743169f75
KonradEcon <konrad.dierks@ecomail.fr>
2026-07-07 11:24:07 +0200

    updated paper revision date, using yarn instead of npm

--- src/components/AppendixTabs.js
+++ src/components/AppendixTabs.js
@@ -67,8 +67,8 @@ const AppendixTabs = ({ defaultTab = 0 }) => {
           [1]
           <h2>Code for the graphics</h2>
           <p>
-            The code for generating the graphics in the paper can be found
-            here:{" "} <br />
+            The code for generating the graphics in the paper can be found here:{" "}
+            <br />
             <span className="inline-link-with-icon">
               <GithubIcon className="link-icon" aria-hidden="true" />
               <a href="https://github.com/KonradEcon/homo-moralis-turnout">
@@ -78,9 +78,9 @@ const AppendixTabs = ({ defaultTab = 0 }) => {
           </p>
           <h2>Interactive plots</h2>
           <p>
-            We provide <a href="#tab1">an interactive plot</a> showing consistent
-            strategies for a wide range of model parameters in our partisan
-            case.
+            We provide <a href="#tab1">an interactive plot</a> showing
+            consistent strategies for a wide range of model parameters in our
+            partisan case.
           </p>
           <p>
             Moreover, we provide <a href="#tab2">an interactive plot</a> that
@@ -90,10 +90,7 @@ const AppendixTabs = ({ defaultTab = 0 }) => {
           <h2>Authors</h2>
           <p>
             <a href="https://ingelaalger.weebly.com/">Ingela Alger</a>,{" "}
-            <a href="https://www.konraddierks.eu/">
-              Konrad Dierks
-            </a>
-            ,{" "}
+            <a href="https://www.konraddierks.eu/">Konrad Dierks</a>,{" "}
             <a href="https://sites.google.com/site/jflaslierhomepage/">
               Jean-François Laslier
             </a>
@@ -102,7 +99,7 @@ const AppendixTabs = ({ defaultTab = 0 }) => {
           <p>
             1. Konrad Dierks, Ingela Alger, and Jean-François Laslier, Does
             universalization ethics justify participation in large elections?,
-            TSE Working Paper, n. 21-1193, February 2021, revised April 2025.
+            TSE Working Paper, n. 21-1193, February 2021, revised May 2026.
             <span className="reference-link-line">
               <a href="https://www.tse-fr.eu/publications/does-universalization-ethics-justify-participation-large-elections-0">
                 https://www.tse-fr.eu/publications/does-universalization-ethics-justify-participation-large-elections-0
